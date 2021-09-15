475c41b26c7adec9afa8172167602a0f9bd06c69
Thiago da Silva <thiagodasilvag1989@gmail.com>
2021-09-15 16:00:49 -0300

    ADD: Model Cliente

---
+++ OrdemServicos/Models/Cliente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrdemServicos.Models
+{
+    public class Cliente
+    {
+        private string Name { get; set; }
+
+        private decimal CPF { get; set; }
+
+        private string Email { get; set; }
+
+        private string Telefone { get; set; }
+
+        private string Celular { get; set; }
+
+        private string Endereco { get; set; }
+
+        private string Cidade { get; set; }
+
+        private string UF { get; set; }
+
+        private string CEP { get; set; }
+
+        public Cliente(string name, decimal cPF, string email, string telefone, string celular, string endereco, string cidade, string uF, string cEP)
+        {
+            Name = name;
+            CPF = cPF;
+            Email = email;
+            Telefone = telefone;
+            Celular = celular;
+            Endereco = endereco;
+            Cidade = cidade;
+            UF = uF;
+            CEP = cEP;
+        }
+
+
+    }
+}
