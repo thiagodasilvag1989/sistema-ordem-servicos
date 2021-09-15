4247c87d339751a3b00ece45ecdcbde2e59f51ad
fabriciopereira <fabriciopereira@bne-empregos.com.br>
2021-09-15 15:49:53 -0300

    ADD: Adicionando estrutura inicial

---
+++ OrdemServicos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OrdemServicos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
